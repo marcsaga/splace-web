f710f65010cae4ecd05d37113e9f229f47d20c98
Marc <marc@haddock.app>
2021-05-16 12:18:49 +0200

    Added calibri & sahar tipography

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -19,7 +19,7 @@ const Header = () => {
       <Container>
         <Fade left={!isMobile} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
-            <span className="text-color-main">{title} Web</span>
+            <span className="text-color-main">{title}</span>
           </h1>
           <h2 className="hero-subtitle">{subtitle}</h2>
         </Fade>
