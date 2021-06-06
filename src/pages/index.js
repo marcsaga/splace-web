8f79608265642650e926c03392bb8b265dbd96e8
Marc <marc@haddock.app>
2021-06-06 17:45:50 +0200

    fix-build-firebase-errro

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import firebase from 'firebase';
 import App from '../components/App';
 import { headData } from '../mock/data';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,11 +8,14 @@ import { firebaseConfig } from '../../firebase.config';
 
 export default () => {
   const { title, lang, description } = headData;
-  if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-  } else {
-    firebase.app(); // if already initialized, use that one
-  }
+
+  import('firebase').then((firebase) => {
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    } else {
+      firebase.app(); // if already initialized, use that one
+    }
+  });
   return (
     <>
       <Helmet>
